f681b2523b33af000c08825ea17d24fb8bafa5c7
Juda Kaleta <yettycz@gmail.com>
2010-08-25 13:21:17 +0200

    Functionality to getting HTML from files.

--- server.py
+++ server.py
@@ -1,21 +1,34 @@
 import socket
 
-host = ''
-port = 8080
+def getHtmlPage(name):
+	try:
+		f = open('html/'+name+'.html')
+	except:
+		return getHtmlPage('error404')
+	return f.read()
 
-serverSocket = socket.socket()
-serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-serverSocket.bind((host, port))
-serverSocket.listen(1)
+def sendPage(socket, name):
+	page = getHtmlPage(name)
+	socket.send("HTTP/1.1 200 OK\nContent-Type: text/html; charset=UTF-8\nContent-Length: "+str(len(page))+"\n\n")
+	socket.send(page)
 
-while 1:
-  print "Wait for client"
-  clientSocket, client = serverSocket.accept()
-  print client, " is connected!"
-  requestClient = clientSocket.recv(1024) 
-  if ("GET" in requestClient): #if request from client is GET
-    clientSocket.send("HTTP/1.1 200 OK\nContent-Type: text/html; charset=UTF-8\nContent-Length: 71\n\n") #HTML head
-    clientSocket.send("<html><head><title>Golem</title></head><body><p>GOLEM</p></body></html>") #HTML page
-  clientSocket.close()
-       
-                                                                                                            
+if __name__=="__main__":
+	host = ''
+	port = 8080
+
+	serverSocket = socket.socket()
+	serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	serverSocket.bind((host, port))
+	serverSocket.listen(1)
+
+	while 1:
+		print "Wait for client"
+
+		clientSocket, client = serverSocket.accept()
+		print client, " is connected!"
+
+		requestClient = clientSocket.recv(1024)
+		if ("GET" in requestClient): #if request from client is GET
+			sendPage(clientSocket, 'index')
+			print requestClient
+		clientSocket.close()
